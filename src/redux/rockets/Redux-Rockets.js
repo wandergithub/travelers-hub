1d2bc2cfa13e3a5b8aefef8e21864572b399b0bf
wandergithub <wandergonzalez0@hotmail.com>
2022-06-14 12:31:34 -0400

    Add button action to dispatch reservation on rocket

--- src/redux/rockets/Redux-Rockets.js
+++ src/redux/rockets/Redux-Rockets.js
@@ -1,17 +1,26 @@
 import axios from 'axios';
 // Actions
 const FETCHED_DATA = 'FETCHED_DATA';
-
+const RESERVED_ROCKET = 'RESERVED_ROCKET';
 // Reducer
 export default function reducer(state = [], action = {}) {
   switch (action.type) {
     case FETCHED_DATA:
       return action.list;
+    case RESERVED_ROCKET:
+      return [...state.map((rocket) => {
+        if (rocket.id !== action.id) return rocket;
+        return { ...rocket, reserved: true };
+      })];
     default: return state;
   }
 }
 
 // Action Creators
+export const reserveRocket = (id) => ({
+  type: RESERVED_ROCKET,
+  id,
+});
 // thunks
 export const dispatchRockets = () => async (dispatch) => {
   const response = await axios('https://api.spacexdata.com/v3/rockets');
