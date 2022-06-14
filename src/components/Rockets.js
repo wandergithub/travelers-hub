9422dab1c223477210e3b72dacfc5e4bb516850a
wandergithub <wandergonzalez0@hotmail.com>
2022-06-14 11:43:05 -0400

    solve linter

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -6,7 +6,6 @@ import { useSelector } from 'react-redux/es/exports';
 
 export default function Rockets() {
   const rockets = useSelector((state) => state.rockets);
-  console.log(rockets);
   return (
     <Container>
       {
