e2faf2cdd4773b2b896c31e4ec1c2b8f8500585c
wandergithub <wandergonzalez0@hotmail.com>
2022-06-14 09:40:23 -0400

    Install Bootstrap

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -4,7 +4,6 @@ import { useSelector } from 'react-redux/es/exports';
 export default function Missions() {
   const missions = useSelector((state) => state.missions);
   console.log(missions);
-
   return (
     <div>
       <h1>Missions</h1>
